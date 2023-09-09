88581245eb7cf627c1f8534f8aa6ee5e27ead639
AlmasIV <Almas.Kairbek.AK@gmail.com>
2023-09-09 23:29:59 +0600

    Some small changes were made.

---
+++ LinkedListStack/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkedListStack;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ LinkedListStack/Stack.cs
@@ -0,0 +1,18 @@
+namespace LinkedListStack;
+
+public class Stack<T> : IStack<T>
+{
+    public bool IsEmpty => throw new NotImplementedException();
+
+    public int Length => throw new NotImplementedException();
+
+    public T Pop()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Push(T item)
+    {
+        throw new NotImplementedException();
+    }
+}
