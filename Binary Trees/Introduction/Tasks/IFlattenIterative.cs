d9be4323c449675ddc8d2372130d32e84cdd5193
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-04-17 18:19:23 +0500

    Extracted flattening binary trees' logic into interfaces

---
+++ Binary Trees/Introduction/Tasks/IFlattenIterative.cs
@@ -0,0 +1,22 @@
+namespace Introduction.Tasks;
+
+/// <summary>
+///     Enables flattenning a binary tree. Assumes that a developer implements an iterative approach.
+/// </summary>
+public interface IFlattenIterative {
+    /// <summary>
+    ///     Flattens a binary tree starting from the root node, and returns the values in a list.
+    /// </summary>
+    /// <remarks>
+    ///     Uses iterative approach.
+    /// </remarks>
+    /// <param name="rootNode">
+    ///     The root node of a binary tree.
+    /// </param>
+    /// <returns>
+    ///     The list of values of the flattened binary tree.
+    /// </returns>
+    public static List<T> FlattenIterative<T>(Node<T> rootNode){
+        throw new NotImplementedException();
+    }
+}
---
+++ Binary Trees/Introduction/Tasks/IFlattenRecursive.cs
@@ -0,0 +1,22 @@
+namespace Introduction.Tasks;
+
+/// <summary>
+///     Enables flattenning a binary tree. Assumes that a developer implements a recursive approach.
+/// </summary>
+public interface IFlattenRecursive {
+    /// <summary>
+    ///     Flattens a binary tree starting from the root node, and returns the values in a list.
+    /// </summary>
+    /// <remarks>
+    ///     Uses recursive approach.
+    /// </remarks>
+    /// <param name="rootNode">
+    ///     The root node of a binary tree.
+    /// </param>
+    /// <returns>
+    ///     The list of values of the flattened binary tree.
+    /// </returns>
+    public static List<T> FlattenRecursive<T>(Node<T> rootNode){
+        throw new NotImplementedException();
+    }
+}
